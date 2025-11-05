c637380507b1e43f7d197ac7e2744061c03235ba
Nahida Akther <ummenahida20@gmail.com>
2025-11-05 16:14:47 +0600

    create common layout

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,30 +1,42 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import path from 'path';
+import {jwtDecode} from 'jwt-decode';
  
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
 
-//   const token = request.cookies.get('token')?.value;
-  const token = request.cookies.get('accessToken')?.value;
+interface userPayload {
+    id: string;
+    name: string;
+    email: string;
+    role: 'ADMIN' | 'DOCTOR' | 'PATIENT' | 'guest';
+    iat: number;
+    exp: number;
+}
+
+const authRoutes = ["/login", "/register", "/forgot-password"];
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+
+  const accessToken = request.cookies.get('accessToken')?.value;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
 //   console.log("proxy accessToken:", accessToken)
-  console.log("proxy Token:", token)
 
   const {pathname} = request.nextUrl;
 
-  const protectedPaths = ['/dashboard/*','appointments', '/profile/*', 'settings/*']
-  const authPaths = ['/login','/register', '/forgot-password']
 
-  const isAuthPath = authPaths.some((path) => pathname.startsWith(path));
-
-  const isProtectedPath = protectedPaths.some((path) => pathname.startsWith(path));
-
-  if( isProtectedPath && !token){
-    return NextResponse.redirect(new URL('/login', request.url))
+  if(!accessToken && !refreshToken && !authRoutes.includes(pathname)){
+    return NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url))
   }
 
-  if( isAuthPath && token){
+  let user;
+
+  if(accessToken){
+    try{
+      user = jwtDecode(accessToken)
     return NextResponse.redirect(new URL('/', request.url))
+    }catch(err){
+      console.log(err)
+    }
   }
 
   
