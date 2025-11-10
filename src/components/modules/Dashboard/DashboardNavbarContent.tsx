ac3f8dedce46136a4c67dffecae099b9410879f0
Nahida Akther <ummenahida20@gmail.com>
2025-11-11 03:34:51 +0600

    added search bar in dashboard content

--- src/components/modules/Dashboard/DashboardNavbarContent.tsx
+++ src/components/modules/Dashboard/DashboardNavbarContent.tsx
@@ -34,7 +34,7 @@ const DashboardNavbarContent = ({userInfo}:IDashboardNavbarContentProps) => {
         <div className="flex-1">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-            <Input type="search" placeholder="Search..." className="pl-9" />
+            <Input type="search" placeholder="Search Here..." className="pl-9" />
           </div>
         </div>
 
