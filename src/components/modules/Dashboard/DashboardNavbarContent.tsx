2c2c1de92f15f03691f30a594a607437b3488139
Nahida Akther <ummenahida20@gmail.com>
2025-11-11 03:35:29 +0600

    added notification icon

--- src/components/modules/Dashboard/DashboardNavbarContent.tsx
+++ src/components/modules/Dashboard/DashboardNavbarContent.tsx
@@ -43,7 +43,7 @@ const DashboardNavbarContent = ({userInfo}:IDashboardNavbarContentProps) => {
            {/* Notifications  */}
           <Button variant="outline" size="icon" className="relative">
             <Bell className="h-5 w-5" />
-            <span className="absolute top-0 right-0 h-2 w-2 rounded-full bg-red-500" />
+            <span className="absolute top-0 right-0 h-2 w-2 rounded-full bg-red-600" />
           </Button>
 
           {/* User Dropdown */}
