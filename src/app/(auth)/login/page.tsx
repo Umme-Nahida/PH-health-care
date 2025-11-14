6ab09f99f12974a4944284684d89607782147718
Nahida Akther <ummenahida20@gmail.com>
2025-11-15 01:49:26 +0600

    protecting and authorizing the pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
 import LoginForm from "@/components/Auth/login-form";
 
 
-const LoginPage = () => {
+const LoginPage = async({searchParams}: {searchParams: Promise<{redirect: string}>}) => {
+
+  const params = await searchParams;
+
+  console.log("params", params)
+   
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
@@ -11,7 +16,7 @@ const LoginPage = () => {
             Enter your credentials to access your account
           </p>
         </div>
-        <LoginForm />
+        <LoginForm redirect={params.redirect}  />
       </div>
     </div>
   );
