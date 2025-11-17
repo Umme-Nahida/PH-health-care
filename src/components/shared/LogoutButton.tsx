8989d82fc52f39d135fa1d0be8d6df928f6aa972
Nahida Akther <ummenahida20@gmail.com>
2025-11-17 22:08:31 +0600

    create logout button

--- src/components/shared/LogoutButton.tsx
+++ src/components/shared/LogoutButton.tsx
@@ -1,15 +1,21 @@
 "use client"
+import { toast } from 'sonner'
 import { Button } from '../ui/button'
 import { logOut } from '@/services/auth/logOut'
 
 const LogoutButton = () => {
 
-  const handleLogout = async()=>{
-     await logOut()
+  const handleLogout = async () => {
+    await logOut()
   }
   return (
-  <Button  onClick={handleLogout}>Logout</Button>
-)
+    <div>
+      <Button onClick={handleLogout}>Logout</Button>
+
+      <Button onClick={() => toast.success("data is added here")}>Click me</Button>
+
+    </div>
+  )
 }
 
 export default LogoutButton
