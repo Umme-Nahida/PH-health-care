41b781f6f1b0c47b105ea4847508bc0dcce0095d
Nahida Akther <ummenahida20@gmail.com>
2025-11-25 23:18:00 +0600

    configure the next config file

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,34 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+//   images:{
+//     remotePatterns:[
+//       {
+//         protocol:"http",
+//         hostname:"res.cloudinary.com"
+//       }
+//     ]
+//   }
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "res.cloudinary.com",
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+export default nextConfig; 
